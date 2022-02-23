56f300d2eab095b75674f6eab1aa0b615ccc0b2f
Leo(Yi-Chen) Liu <55065942+leo19900723@users.noreply.github.com>
2022-02-22 16:57:14 -0800

    Namespace first creation (#1)
    Co-authored-by: Leo Liu <lyichen@amazon.com>

---
+++ aws-redshiftserverless-namespace/src/main/java/software/amazon/redshiftserverless/namespace/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.redshiftserverless.namespace;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-redshiftserverless-namespace.json");
+    }
+}
