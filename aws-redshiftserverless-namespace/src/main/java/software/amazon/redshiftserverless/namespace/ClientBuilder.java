2beadde2bc01b444beb39d8e931320c0a3a3958a
Emily Zhengyu Sun <100100603+EmilySun-2022@users.noreply.github.com>
2022-06-16 11:45:54 -0700

    Add devo endpoint back for pipeline deployment (#10)

--- aws-redshiftserverless-namespace/src/main/java/software/amazon/redshiftserverless/namespace/ClientBuilder.java
+++ aws-redshiftserverless-namespace/src/main/java/software/amazon/redshiftserverless/namespace/ClientBuilder.java
@@ -8,9 +8,12 @@ import software.amazon.cloudformation.LambdaWrapper;
 import java.net.URI;
 
 public class ClientBuilder {
+  public static final String CLIENT_ENDPOINT_DEVO = "https://devo.us-east-1.serverless.redshift.aws.a2z.com";
   public static RedshiftServerlessClient getClient() {
     return RedshiftServerlessClient.builder()
             .httpClient(LambdaWrapper.HTTP_CLIENT)
+            .region(Region.US_EAST_1)
+            .endpointOverride(URI.create(CLIENT_ENDPOINT_DEVO))
             .build();
   }
 }
