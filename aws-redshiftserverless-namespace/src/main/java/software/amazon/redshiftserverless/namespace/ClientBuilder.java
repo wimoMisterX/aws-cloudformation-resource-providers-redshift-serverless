579d518816f9bf79037e3b10582bf202cb627a8b
Emily Zhengyu Sun <100100603+EmilySun-2022@users.noreply.github.com>
2022-06-21 10:32:29 -0700

    Remove devo endpoint and update mvn repo for public SDK (#12)

--- aws-redshiftserverless-namespace/src/main/java/software/amazon/redshiftserverless/namespace/ClientBuilder.java
+++ aws-redshiftserverless-namespace/src/main/java/software/amazon/redshiftserverless/namespace/ClientBuilder.java
@@ -1,19 +1,12 @@
 package software.amazon.redshiftserverless.namespace;
 
-
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.redshiftserverless.RedshiftServerlessClient;
 import software.amazon.cloudformation.LambdaWrapper;
 
-import java.net.URI;
-
 public class ClientBuilder {
-  public static final String CLIENT_ENDPOINT_DEVO = "https://devo.us-east-1.serverless.redshift.aws.a2z.com";
   public static RedshiftServerlessClient getClient() {
     return RedshiftServerlessClient.builder()
             .httpClient(LambdaWrapper.HTTP_CLIENT)
-            .region(Region.US_EAST_1)
-            .endpointOverride(URI.create(CLIENT_ENDPOINT_DEVO))
             .build();
   }
 }
