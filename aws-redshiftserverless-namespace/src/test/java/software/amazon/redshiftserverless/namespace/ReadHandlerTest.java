74a179166c0c0eb328740e32457f729983d467f7
Don Athukorala <donwimo@amazon.com>
2024-02-13 15:34:56 +0800

    Added tests for CRC handlers

--- aws-redshiftserverless-namespace/src/test/java/software/amazon/redshiftserverless/namespace/ReadHandlerTest.java
+++ aws-redshiftserverless-namespace/src/test/java/software/amazon/redshiftserverless/namespace/ReadHandlerTest.java
@@ -3,6 +3,7 @@ package software.amazon.redshiftserverless.namespace;
 import java.lang.reflect.Method;
 import java.time.Duration;
 import java.util.stream.Stream;
+import java.util.Collections;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -17,6 +18,8 @@ import software.amazon.awssdk.services.redshiftserverless.model.GetNamespaceRequ
 import software.amazon.cloudformation.exceptions.CfnGeneralServiceException;
 import software.amazon.cloudformation.exceptions.CfnInvalidRequestException;
 import software.amazon.awssdk.services.redshiftserverless.model.ListSnapshotCopyConfigurationsRequest;
+import software.amazon.awssdk.services.redshiftserverless.model.ListSnapshotCopyConfigurationsResponse;
+import software.amazon.awssdk.services.redshiftserverless.model.SnapshotCopyConfiguration;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ProgressEvent;
@@ -30,6 +33,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.any;
 
@@ -178,6 +182,49 @@ public class ReadHandlerTest extends AbstractTestBase {
 
         final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, redshiftProxyClient, logger);
 
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModel()).isEqualTo(responseResourceModel);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+    }
+
+    @Test
+    public void handleRequest_ListSnapshotCopyConfigurations() {
+        final ReadHandler handler = new ReadHandler();
+
+        final ResourceModel requestResourceModel = getNamespaceRequestResourceModel();
+        final ResourceModel responseResourceModel = getNamespaceResponseResourceModel().toBuilder()
+                .snapshotCopyConfigurations(Collections.singletonList(software.amazon.redshiftserverless.namespace.SnapshotCopyConfiguration.builder()
+                        .destinationRegion("us-west-2")
+                        .destinationKmsKeyId("AWS_OWNED_KMS_KEY")
+                        .snapshotRetentionPeriod(-1)
+                        .build()))
+                .build();
+
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(requestResourceModel)
+                .build();
+
+        when(proxyClient.client().listSnapshotCopyConfigurations(any(ListSnapshotCopyConfigurationsRequest.class)))
+                .thenReturn(ListSnapshotCopyConfigurationsResponse.builder()
+                        .snapshotCopyConfigurations(Collections.singletonList(SnapshotCopyConfiguration.builder()
+                                .snapshotCopyConfigurationId("snap-id-1234")
+                                .destinationRegion("us-west-2")
+                                .namespaceName(requestResourceModel.getNamespaceName())
+                                .destinationKmsKeyId("AWS_OWNED_KMS_KEY")
+                                .snapshotRetentionPeriod(-1)
+                                .build()))
+                        .build());
+        when(proxyClient.client().getNamespace(any(GetNamespaceRequest.class))).thenReturn(getNamespaceResponseSdk());
+        when(redshiftProxyClient.client().getResourcePolicy(any(GetResourcePolicyRequest.class))).thenReturn(getEmptyResourcePolicyResponseSdk());
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, redshiftProxyClient, logger);
+
+        verify(proxyClient.client(), times(1)).listSnapshotCopyConfigurations(any(ListSnapshotCopyConfigurationsRequest.class));
         assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
